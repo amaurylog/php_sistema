8f6c0987e35bc33578e702f1087ed74a0370f19f
amaurylog <alan1245@live.com>
2017-12-15 17:37:30 -0600

    Conexión a Bases de Datos
    Se agregan algunas funciones al archivo, para poder hacer consultas SQL de una manera sencilla.

---
+++ config/global.php
@@ -0,0 +1,21 @@
+<?php 
+
+	// IP del Servidor
+	define("DB_HOST", "localhost");
+
+	// Nombre de la Base de Datos
+	define('DB_NAME', 'dbsistema');
+	
+	// Usuario de la base de Datos
+	define('DB_USERNAME', 'root');
+
+	// Contraseña del usuario de la base de datos
+	define('DB_PASSWORD', '');
+
+	// Definir la Codificacion de los Caracteres
+	define('DB_ENCODE', 'utf-8');
+
+	// Definir una constante para el nombre del proyecto
+	define('PRO_NOMBRE', 'DarbiShop');
+	
+ ?>
